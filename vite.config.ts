cebc27dffb2bf085073458d1892256b2d5ae899f
nvision <16458878+nvisional@user.noreply.gitee.com>
2025-12-10 16:01:13 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: 'Ms.Qi',
+    base: '/Ms.Qi/',
     build: {
       outDir: 'docs',
     },
